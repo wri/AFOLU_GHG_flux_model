c96a03e2190a89b977c86bd9d4924a5703ff1bb3
erin-glen <erin.glen@wri.org>
2024-05-16 11:01:36 -0600

    Changes to structure, addition of new python scripts for preprocessing

--- src/preprocessing/pp_dadap.py
+++ src/preprocessing/pp_dadap.py
@@ -1,97 +1,83 @@
-"""
-Load dadap density data, resample to correct grid, and reclassify to binary
-"""
-
-import rioxarray
-import rasterio
-import xarray as xr
-import pandas as pd
-import pycountry
 import os
-from dask.distributed import Client
-from src.utils import universal_util as uu
+import rioxarray
+import boto3
+import logging
+import time
+import subprocess
 
-# create a dask cluster and make this notebook its client
-# client = get_dask_ecs_client(n_workers=100)
-# client
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Print the current working directory
-print("Current working directory:", os.getcwd())
-print("Files in the current directory:", os.listdir())
+# AWS S3 setup
+s3_client = boto3.client('s3')
+s3_bucket_name = 'gfw2-data'
+s3_tiles_prefix = 'climate/carbon_model/other_emissions_inputs/peatlands/processed/20230315/'
+s3_output_prefix = 'climate/AFOLU_flux_model/organic_soils/inputs/processed/dadap_density/'
 
-tiles_test = ["00N_110E.tif"]
-tiles_full = ["00N_000E.tif", "00N_010E.tif", "00N_020E.tif", "00N_030E.tif", "00N_040E.tif", "00N_040W.tif", "00N_050W.tif", "00N_060W.tif", "00N_070W.tif", "00N_080W.tif", "00N_090E.tif", "00N_090W.tif", "00N_100E.tif", "00N_110E.tif", "00N_120E.tif", "00N_130E.tif", "00N_140E.tif", "00N_150E.tif", "00N_160E.tif", "10N_000E.tif", "10N_010E.tif", "10N_010W.tif", "10N_020E.tif", "10N_020W.tif", "10N_030E.tif", "10N_040E.tif", "10N_050E.tif", "10N_050W.tif", "10N_060W.tif", "10N_070E.tif", "10N_070W.tif", "10N_080E.tif", "10N_080W.tif", "10N_090E.tif", "10N_090W.tif", "10N_100E.tif", "10N_110E.tif", "10N_120E.tif", "10N_130E.tif", "10N_140E.tif", "10S_010E.tif", "10S_020E.tif", "10S_030E.tif", "10S_040E.tif", "10S_040W.tif", "10S_050E.tif", "10S_050W.tif", "10S_060W.tif", "10S_070W.tif", "10S_080W.tif", "10S_100E.tif", "10S_110E.tif", "10S_120E.tif", "10S_130E.tif", "10S_140E.tif", "10S_150E.tif", "10S_160E.tif", "10S_170E.tif", "20N_000E.tif", "20N_010E.tif", "20N_010W.tif", "20N_020E.tif", "20N_020W.tif", "20N_030E.tif", "20N_040E.tif", "20N_050E.tif", "20N_060W.tif", "20N_070E.tif", "20N_070W.tif", "20N_080E.tif", "20N_080W.tif", "20N_090E.tif", "20N_090W.tif", "20N_100E.tif", "20N_100W.tif", "20N_110E.tif", "20N_110W.tif", "20N_120E.tif", "20N_120W.tif", "20S_010E.tif", "20S_020E.tif", "20S_030E.tif", "20S_040E.tif", "20S_050W.tif", "20S_060W.tif", "20S_070W.tif", "20S_080W.tif", "20S_110E.tif", "20S_120E.tif", "20S_130E.tif", "20S_140E.tif", "20S_150E.tif", "20S_160E.tif", "30N_010W.tif", "30N_020W.tif", "30N_030E.tif", "30N_040E.tif", "30N_050E.tif", "30N_060E.tif", "30N_070E.tif", "30N_080E.tif", "30N_080W.tif", "30N_090E.tif", "30N_090W.tif", "30N_100E.tif", "30N_100W.tif", "30N_110E.tif", "30N_110W.tif", "30N_120E.tif", "30N_120W.tif", "30S_010E.tif", "30S_020E.tif", "30S_030E.tif", "30S_060W.tif", "30S_070W.tif", "30S_080W.tif", "30S_090W.tif", "30S_110E.tif", "30S_120E.tif", "30S_130E.tif", "30S_140E.tif", "30S_150E.tif", "30S_170E.tif", "40N_000E.tif", "40N_010E.tif", "40N_010W.tif", "40N_020E.tif", "40N_030E.tif", "40N_040E.tif", "40N_050E.tif", "40N_060E.tif", "40N_070E.tif", "40N_070W.tif", "40N_080E.tif", "40N_080W.tif", "40N_090E.tif", "40N_090W.tif", "40N_100E.tif", "40N_100W.tif", "40N_110E.tif", "40N_110W.tif", "40N_120E.tif", "40N_120W.tif", "40N_130E.tif", "40N_130W.tif", "40N_140E.tif", "40S_070W.tif", "40S_080W.tif", "40S_140E.tif", "40S_160E.tif", "40S_170E.tif", "50N_000E.tif", "50N_010E.tif", "50N_010W.tif", "50N_020E.tif", "50N_030E.tif", "50N_040E.tif", "50N_050E.tif", "50N_060W.tif", "50N_070E.tif", "50N_070W.tif", "50N_080E.tif", "50N_080W.tif", "50N_090E.tif", "50N_090W.tif", "50N_100E.tif", "50N_100W.tif", "50N_110E.tif", "50N_110W.tif", "50N_120E.tif", "50N_120W.tif", "50N_130E.tif", "50N_130W.tif", "50N_140E.tif", "50N_150E.tif", "50S_060W.tif", "50S_070W.tif", "50S_080W.tif", "60N_000E.tif", "60N_010E.tif", "60N_010W.tif", "60N_020E.tif", "60N_020W.tif", "60N_030E.tif", "60N_040E.tif", "60N_050E.tif", "60N_060E.tif", "60N_060W.tif", "60N_070E.tif", "60N_070W.tif", "60N_080E.tif", "60N_080W.tif", "60N_090E.tif", "60N_090W.tif", "60N_100E.tif", "60N_100W.tif", "60N_110E.tif", "60N_110W.tif", "60N_120E.tif", "60N_120W.tif", "60N_130W.tif", "60N_140E.tif", "60N_140W.tif", "60N_150E.tif", "60N_150W.tif", "60N_160E.tif", "60N_160W.tif", "60N_170E.tif", "60N_170W.tif", "60N_180W.tif", "70N_000E.tif", "70N_010E.tif", "70N_010W.tif", "70N_020E.tif", "70N_020W.tif", "70N_030E.tif", "70N_030W.tif", "70N_040E.tif", "70N_050E.tif", "70N_060E.tif", "70N_070E.tif", "70N_080E.tif", "70N_080W.tif", "70N_090E.tif", "70N_090W.tif", "70N_100E.tif", "70N_100W.tif", "70N_110E.tif", "70N_110W.tif", "70N_120E.tif", "70N_120W.tif", "70N_130E.tif", "70N_130W.tif", "70N_140E.tif", "70N_140W.tif", "70N_150E.tif", "70N_150W.tif", "70N_160E.tif", "70N_160W.tif", "70N_170E.tif", "70N_180W.tif", "80N_020E.tif", "80N_060E.tif", "80N_070E.tif", "80N_080E.tif", "80N_090E.tif", "80N_110E.tif", "80N_120E.tif", "80N_120W.tif", "80N_130W.tif", "80N_140E.tif", "80N_140W.tif"]
+# Local paths
+tiles_dir = r"C:\GIS\Temp\GFW_Global_Peatlands"
+output_dir = r"C:\GIS\Data\Global\Wetlands\Processed\dadap"
+dadap_density_raw = r"C:\GIS\Data\Global\Wetlands\Raw\Tropics\canal_length_data\canal_length_data\canal_length_1km.tif"
+tiles_list = ["00N_090E", "00N_100E", "00N_110E", "10N_090E", "10N_100E", "10N_110E"]
 
-# specify full or test:
-tiles = tiles_test
+def delete_local_file(file_path):
+    max_attempts = 5
+    for attempt in range(max_attempts):
+        try:
+            os.remove(file_path)
+            logging.info(f"Deleted local file {file_path}")
+            break
+        except PermissionError as e:
+            if attempt < max_attempts - 1:
+                logging.warning(f"Failed to delete {file_path}, retrying... ({e})")
+                time.sleep(1)  # Wait a bit before retrying
+            else:
+                logging.error(f"Could not delete {file_path} after several attempts. ({e})")
 
-peatlands_uri = "s3://gfw-data-lake/gfw_peatlands/v20230302/raster/epsg-4326/10/40000/is/geotiff/"
-pixel_area_uri = "s3://gfw-data-lake/gfw_pixel_area/v20150327/raster/epsg-4326/10/40000/m2/geotiff/"
-dadap_uri = "s3://gfw2-data/climate/AFOLU_flux_model/organic_soils/inputs/raw/Dadap_SEA_Drainage/canal_length_data/canal_length_1km.tif"
+def handle_file_operations(tile_name):
+    local_tile_path = os.path.join(tiles_dir, f"{tile_name}.tif")
+    local_output_path = os.path.join(output_dir, f"dadap_{tile_name}.tif")
+    s3_tile_path = f"{s3_tiles_prefix}{tile_name}_peat_mask_processed.tif"
+    s3_output_path = f"{s3_output_prefix}dadap_{tile_name}.tif"
 
-s3_base_dir = "s3://gfw2-data/climate/AFOLU_flux_model/organic_soils/"
-dadap_pattern = "dadap_density"
-dadap_dir = os.path.join(s3_base_dir,"inputs/processed/dadap_density/")
+    if os.path.exists(local_output_path):
+        logging.info(f"Output file {local_output_path} already exists, skipping processing.")
+        return
 
-def get_tile_dataset(uri, tile, name, template=None):
-    try:
-        return rioxarray.open_rasterio(uri + tile, chunks=4000, default_name=name).squeeze("band")
-    except rasterio.errors.RasterioIOError as e:
-        if template is not None:
-            return xr.zeros_like(template)
-        else:
-            raise e
-
-def coords(tile_id):
-    NS = tile_id.split("_")[0][-1:]
-    EW = tile_id.split("_")[1][-1:]
-
-    if NS == 'S':
-        ymax =-1*int(tile_id.split("_")[0][:2])
+    if not os.path.exists(local_tile_path):
+        try:
+            s3_client.download_file(s3_bucket_name, s3_tile_path, local_tile_path)
+            logging.info(f"Downloaded {s3_tile_path} to {local_tile_path}")
+        except Exception as e:
+            logging.error(f"Failed to download {s3_tile_path}. Error: {e}")
+            return
     else:
-        ymax = int(str(tile_id.split("_")[0][:2]))
+        logging.info(f"Tile {local_tile_path} already exists locally, skipping download.")
 
-    if EW == 'W':
-        xmin = -1*int(str(tile_id.split("_")[1][:3]))
-    else:
-        xmin = int(str(tile_id.split("_")[1][:3]))
-
-
-    ymin = str(int(ymax) - 10)
-    xmax = str(int(xmin) + 10)
-
-    return xmin, ymin, xmax, ymax
-
-def get_dataset(uri, name, template=None):
     try:
-        return rioxarray.open_rasterio(uri, chunks=4000, default_name=name).squeeze("band")
-    except rasterio.errors.RasterioIOError as e:
-        if template is not None:
-            return xr.zeros_like(template)
-        else:
-            raise e
+        tile_ds = rioxarray.open_rasterio(local_tile_path, masked=True)
+        dadap_ds = rioxarray.open_rasterio(dadap_density_raw, masked=True)
+        clipped_dadap = dadap_ds.rio.clip_box(*tile_ds.rio.bounds())
+        clipped_dadap = clipped_dadap.rio.reproject_match(tile_ds)
+        clipped_dadap.rio.to_raster(local_output_path)
+        logging.info(f"Processed and saved locally {local_output_path}")
+    finally:
+        if os.path.exists(local_tile_path):
+            try:
+                os.remove(local_tile_path)
+                logging.info(f"Deleted local file {local_tile_path}")
+            except PermissionError as e:
+                logging.error(f"Could not delete {local_tile_path}. Error: {e}")
 
-def print_raster_info(raster):
-    """
-    Print detailed information about a rioxarray object.
+os.makedirs(tiles_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
 
-    :param raster: rioxarray object to get information from
-    """
+for tile in tiles_list:
+    handle_file_operations(tile)
 
-    # Basic raster information
-    print("Raster Information:")
-    print(f"  Name: {raster.name if hasattr(raster, 'name') else 'Not set'}")  # Default name for the raster
-    print(f"  Dimensions: {raster.dims}")  # Names of the dimensions (e.g., x, y, band)
-    print(f"  Shape: {raster.shape}")  # Shape of the raster (e.g., (1, 512, 512))
-    print(f"  CRS: {raster.rio.crs}")  # Coordinate Reference System
-    print(f"  Bounds: {raster.rio.bounds()}")  # Spatial extent (xmin, ymin, xmax, ymax)
-
-    # Additional raster metadata
-    print("Raster Metadata:")
-    print(f"  Count: {raster.rio.count}")  # Number of bands in the raster
-    print(f"  Data Type: {type(raster)}")  # Data type of the raster bands
-    print(f"  NoData Value: {raster.rio.nodata}")  # NoData value indicating missing data
-
-dadap_density = get_dataset(dadap_uri, "dadap_density", template=None)
-print_raster_info(dadap_density)
-
+# After processing all tiles, upload the entire directory to S3
+local_directory = output_dir
+s3_output_location = f"s3://{s3_bucket_name}/{s3_output_prefix}"
+upload_command = ["aws", "s3", "cp", local_directory, s3_output_location, "--recursive"]
+subprocess.run(upload_command, check=True)
+logging.info(f"Uploaded all processed tiles from {local_directory} to {s3_output_location}")
---
+++ src/preprocessing/pp_extraction.py
@@ -0,0 +1,5 @@
+"""
+This script will process all extraction input datasets into a binary extraction mask
+Extraction datasets may be used as one condition in determining drainage
+Extraction datasets will be used in disaggregating emission factors in drained organic soils
+"""
