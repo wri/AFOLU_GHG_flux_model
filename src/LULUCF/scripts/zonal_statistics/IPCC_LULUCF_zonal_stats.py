4ba3783e28cd2bcc1c2dc25cc156e7387766ac90
dagibbs22 <dagibbs22@gmail.com>
2026-07-02 21:18:17 -0400

    Ran Xu regrowth sensitivity analysis.

--- src/LULUCF/scripts/zonal_statistics/IPCC_LULUCF_zonal_stats.py
+++ src/LULUCF/scripts/zonal_statistics/IPCC_LULUCF_zonal_stats.py
@@ -33,6 +33,8 @@ Notes:
         Contextual layers = IPCC class, node, change and summary, land state node, continent/ecozone, driver, primary/IFL
     - Toook 19 minutes to run for Central Africa (8 tiles) with 50 workers (128 credits, $7)
 
+#TODO Include CO2-only gross emissions as analysis layer
+#TODO Include net mineral soil and organic soil gross emission as analysis layers
 """
 
 import argparse
